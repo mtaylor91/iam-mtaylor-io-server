6b9518733f43550224033fb54bace2ddff682890
Mike Taylor <mike.charles.taylor@gmail.com>
2024-02-07 23:35:18 -0700

    Minor refactor

--- src/Lib/API.hs
+++ src/Lib/API.hs
@@ -1,11 +1,17 @@
 {-# LANGUAGE DataKinds       #-}
 {-# LANGUAGE TypeOperators   #-}
-module Lib.API ( api, usersAPI, groupsAPI, membershipsAPI, API ) where
+module Lib.API
+  ( api
+  , API
+  , UserAPI
+  , GroupAPI
+  , UsersAPI
+  , GroupsAPI
+  , MembershipsAPI
+  ) where
 
 import Servant
 
-import Lib.DB
-import Lib.Handlers
 import Lib.IAM
 
 type API
@@ -39,33 +45,6 @@ type GroupAPI
 type MembershipsAPI
   = ReqBody '[JSON] Membership :> Post '[JSON] Membership
   :<|> ( Capture "group" GroupId :> Capture "user" UserId :> Delete '[JSON] Membership )
-
-usersAPI :: DB db => db -> Server UsersAPI
-usersAPI db
-  = listUsersHandler db
-  :<|> createUserHandler db
-  :<|> userAPI db
-
-userAPI :: DB db => db -> UserId -> Server UserAPI
-userAPI db user
-  = getUserHandler db user
-  :<|> deleteUserHandler db user
-
-groupsAPI :: DB db => db -> Server GroupsAPI
-groupsAPI db
-  = listGroupsHandler db
-  :<|> createGroupHandler db
-  :<|> groupAPI db
-
-groupAPI :: DB db => db -> GroupId -> Server GroupAPI
-groupAPI db group
-  = getGroupHandler db group
-  :<|> deleteGroupHandler db group
-
-membershipsAPI :: DB db => db -> Server MembershipsAPI
-membershipsAPI db
-  = createMembershipHandler db
-  :<|> deleteMembershipHandler db
 
 api :: Proxy API
 api = Proxy
--- src/Lib/Server.hs
+++ src/Lib/Server.hs
@@ -6,6 +6,8 @@ import Servant
 
 import Lib.API
 import Lib.DB
+import Lib.Handlers
+import Lib.IAM
 
 startApp :: DB db => db -> Int -> IO ()
 startApp db port = run port $ app db
@@ -15,3 +17,30 @@ app db = serve api $ server db
 
 server :: DB db => db -> Server API
 server db = usersAPI db :<|> groupsAPI db :<|> membershipsAPI db
+
+usersAPI :: DB db => db -> Server UsersAPI
+usersAPI db
+  = listUsersHandler db
+  :<|> createUserHandler db
+  :<|> userAPI db
+
+userAPI :: DB db => db -> UserId -> Server UserAPI
+userAPI db user
+  = getUserHandler db user
+  :<|> deleteUserHandler db user
+
+groupsAPI :: DB db => db -> Server GroupsAPI
+groupsAPI db
+  = listGroupsHandler db
+  :<|> createGroupHandler db
+  :<|> groupAPI db
+
+groupAPI :: DB db => db -> GroupId -> Server GroupAPI
+groupAPI db group
+  = getGroupHandler db group
+  :<|> deleteGroupHandler db group
+
+membershipsAPI :: DB db => db -> Server MembershipsAPI
+membershipsAPI db
+  = createMembershipHandler db
+  :<|> deleteMembershipHandler db
