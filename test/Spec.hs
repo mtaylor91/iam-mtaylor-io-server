d9b807a4d70543384943ae7660b6b38d2473f4b6
Mike Taylor <mike.charles.taylor@gmail.com>
2024-02-10 01:26:27 -0700

    Fix tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -23,33 +23,30 @@ import Lib.IAM.DB.InMemory
 main :: IO ()
 main = do
   db <- inMemory
-  hspec $ spec db
+  (pk, sk) <- createKeypair
+  let callerId = UserEmail "caller@example.com"
+      callerPrincipal = UserPrincipal callerId pk
+  result <- runExceptT $ createUser db callerPrincipal
+  case result of
+    Right _  -> hspec $ spec db pk sk
+    Left _ -> error "Failed to create caller user"
 
-spec :: DB db => db -> Spec
-spec db = with (return $ app db) $ do
+spec :: DB db => db -> PublicKey -> SecretKey -> Spec
+spec db callerPK callerSK = with (return $ app db) $ do
   describe "GET /users" $ do
     it "responds with 200" $ do
-      (pk, sk) <- liftIO createKeypair
-      let userPrincipal = UserPrincipal (UserEmail "test@example.com") pk
-      r1 <- liftIO $ runExceptT $ createUser db userPrincipal
-      liftIO $ r1 `shouldBe` Right userPrincipal
       let headers =
-            [ ("X-User-Id", "test@example.com")
-            , ("X-Public-Key", encodeUtf8 $ encodeBase64 $ unPublicKey pk)
+            [ ("X-User-Id", "caller@example.com")
+            , ("X-Public-Key", encodeUtf8 $ encodeBase64 $ unPublicKey callerPK)
             , ("Authorization", "Signature " <> sig)
             ]
-          sig = encodeUtf8 $ encodeBase64 $ unSignature $ dsign sk stringToSign
+          sig = encodeUtf8 $ encodeBase64 $ unSignature $ dsign callerSK stringToSign
           stringToSign = authStringToSign methodGet "/users" ""
       request methodGet "/users" headers mempty `shouldRespondWith` 200
-      r2 <- liftIO $ runExceptT $ deleteUser db $ UserEmail "test@example.com"
-      liftIO $ r2 `shouldBe` Right (UserEmail "test@example.com")
   describe "POST /users with email address" $ do
     it "responds with 201" $ do
       (pk, _) <- liftIO createKeypair
-      (callerPK, callerSK) <- liftIO createKeypair
-      let callerId = UserEmail "caller@example.com"
-          callerPrincipal = UserPrincipal callerId callerPK
-          uid = UserEmail "bob@example.com"
+      let uid = UserEmail "bob@example.com"
           user = UserPrincipal uid pk
           userJSON = encode user
           headers =
@@ -60,21 +57,14 @@ spec db = with (return $ app db) $ do
             ]
           sig = encodeUtf8 $ encodeBase64 $ unSignature $ dsign callerSK stringToSign
           stringToSign = authStringToSign methodPost "/users" ""
-      r1 <- liftIO $ runExceptT $ createUser db callerPrincipal
-      liftIO $ r1 `shouldBe` Right callerPrincipal
       request methodPost "/users" headers userJSON `shouldRespondWith` 201
-      r2 <- liftIO $ runExceptT $ deleteUser db uid
-      liftIO $ r2 `shouldBe` Right uid
-      r3 <- liftIO $ runExceptT $ deleteUser db callerId
-      liftIO $ r3 `shouldBe` Right callerId
+      result <- liftIO $ runExceptT $ deleteUser db uid
+      liftIO $ result `shouldBe` Right uid
   describe "POST /users with UUID" $ do
     it "responds with 201" $ do
       uuid <- liftIO nextRandom
       (pk, _) <- liftIO createKeypair
-      (callerPK, callerSK) <- liftIO createKeypair
-      let callerId = UserEmail "caller@example.com"
-          callerPrincipal = UserPrincipal callerId callerPK
-          user = UserPrincipal (UserUUID uuid) pk
+      let user = UserPrincipal (UserUUID uuid) pk
           userJSON = encode user
           headers =
             [ ("Content-Type", "application/json")
@@ -84,33 +74,57 @@ spec db = with (return $ app db) $ do
             ]
           sig = encodeUtf8 $ encodeBase64 $ unSignature $ dsign callerSK stringToSign
           stringToSign = authStringToSign methodPost "/users" ""
-      r1 <- liftIO $ runExceptT $ createUser db callerPrincipal
-      liftIO $ r1 `shouldBe` Right callerPrincipal
       request methodPost "/users" headers userJSON `shouldRespondWith` 201
-      r2 <- liftIO $ runExceptT $ deleteUser db $ UserUUID uuid
-      liftIO $ r2 `shouldBe` Right (UserUUID uuid)
-      r3 <- liftIO $ runExceptT $ deleteUser db callerId
-      liftIO $ r3 `shouldBe` Right callerId
+      result <- liftIO $ runExceptT $ deleteUser db $ UserUUID uuid
+      liftIO $ result `shouldBe` Right (UserUUID uuid)
   describe "GET /groups" $ do
     it "responds with 200" $ do
-      get "/groups" `shouldRespondWith` 200
+      let headers =
+            [ ("X-User-Id", "caller@example.com")
+            , ("X-Public-Key", encodeUtf8 $ encodeBase64 $ unPublicKey callerPK)
+            , ("Authorization", "Signature " <> sig)
+            ]
+          sig = encodeUtf8 $ encodeBase64 $ unSignature $ dsign callerSK stringToSign
+          stringToSign = authStringToSign methodGet "/groups" ""
+      request methodGet "/groups" headers mempty `shouldRespondWith` 200
   describe "POST /groups" $ do
     it "responds with 201" $ do
-      let headers = [("Content-Type", "application/json")]
+      let headers =
+            [ ("Content-Type", "application/json")
+            , ("X-User-Id", "caller@example.com")
+            , ("X-Public-Key", encodeUtf8 $ encodeBase64 $ unPublicKey callerPK)
+            , ("Authorization", "Signature " <> sig)
+            ]
           group = Group (GroupName "admins") []
           groupJSON = encode group
+          sig = encodeUtf8 $ encodeBase64 $ unSignature $ dsign callerSK stringToSign
+          stringToSign = authStringToSign methodPost "/groups" ""
       request methodPost "/groups" headers groupJSON `shouldRespondWith` 201
       r <- liftIO $ runExceptT $ deleteGroup db $ GroupName "admins"
       liftIO $ r `shouldBe` Right ()
   describe "GET /policies" $ do
     it "responds with 200" $ do
-      get "/policies" `shouldRespondWith` 200
+      let headers =
+            [ ("X-User-Id", "caller@example.com")
+            , ("X-Public-Key", encodeUtf8 $ encodeBase64 $ unPublicKey callerPK)
+            , ("Authorization", "Signature " <> sig)
+            ]
+          sig = encodeUtf8 $ encodeBase64 $ unSignature $ dsign callerSK stringToSign
+          stringToSign = authStringToSign methodGet "/policies" ""
+      request methodGet "/policies" headers mempty `shouldRespondWith` 200
   describe "POST /policies" $ do
     it "responds with 201" $ do
       pid <- liftIO nextRandom
-      let headers = [("Content-Type", "application/json")]
+      let headers =
+            [ ("Content-Type", "application/json")
+            , ("X-User-Id", "caller@example.com")
+            , ("X-Public-Key", encodeUtf8 $ encodeBase64 $ unPublicKey callerPK)
+            , ("Authorization", "Signature " <> sig)
+            ]
           policy = Policy pid [Rule Allow Read "*", Rule Allow Write "*"]
           policyJSON = encode policy
+          sig = encodeUtf8 $ encodeBase64 $ unSignature $ dsign callerSK stringToSign
+          stringToSign = authStringToSign methodPost "/policies" ""
       request methodPost "/policies" headers policyJSON `shouldRespondWith` 201
       r <- liftIO $ runExceptT $ deletePolicy db pid
       liftIO $ r `shouldBe` Right policy
