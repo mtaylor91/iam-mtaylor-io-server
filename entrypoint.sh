57b3a8764a8906c4c4fbf000991acd05abf5d552
Mike Taylor <mike.charles.taylor@gmail.com>
2024-02-13 23:16:45 -0700

    Fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -eux
-api-mtaylor-io --admin-email $ADMIN_EMAIL --admin-public-key $ADMIN_PUBLIC_KEY
+api-mtaylor-io server --admin-email $ADMIN_EMAIL --admin-public-key $ADMIN_PUBLIC_KEY
