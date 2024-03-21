9d3b7e154d1d5c4135b5ca194ba367c49e6e72ce
Mike Taylor <mike.charles.taylor@gmail.com>
2024-03-21 07:42:58 -0600

    Add get group command

--- src/Lib/Command/Get.hs
+++ src/Lib/Command/Get.hs
@@ -7,24 +7,31 @@ module Lib.Command.Get
 import Data.Text
 import Options.Applicative
 
+import Lib.Command.Get.Group
 import Lib.Command.Get.User
 
 
-newtype GetCommand
-  = UserGet (Maybe Text)
+data GetCommand
+  = GetUser !(Maybe Text)
+  | GetGroup !Text
   deriving (Show)
 
 
 get :: GetCommand -> IO ()
-get (UserGet email') = getUser email'
+get (GetUser email') = getUser email'
+get (GetGroup group') = getGroup group'
 
 
 getCommand :: Parser GetCommand
 getCommand = subparser
-  ( command "user"
+  ( command "group"
     ( info
-      ( UserGet
-        <$> optional ( argument str (metavar "EMAIL") )
+      ( GetGroup <$> argument str (metavar "GROUP")
+      ) (progDesc "Get a group")
+    )
+  <> command "user"
+    ( info
+      ( GetUser <$> optional ( argument str (metavar "EMAIL") )
       ) (progDesc "Get a user")
     )
   )
