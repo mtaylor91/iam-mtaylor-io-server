d0fc8677e7c5081960fad567127ad535da6d4d18
Mike Taylor <mike.charles.taylor@gmail.com>
2024-04-11 15:56:09 -0600

    Implement list sessions command

--- src/IAM/Command/List.hs
+++ src/IAM/Command/List.hs
@@ -8,12 +8,14 @@ import Options.Applicative
 
 import IAM.Command.List.Groups
 import IAM.Command.List.Policies
+import IAM.Command.List.Sessions
 import IAM.Command.List.Users
 
 
 data ListCommand
   = ListGroups !ListGroupsOptions
   | ListPolicies !ListPoliciesOptions
+  | ListSessions !ListSessionsOptions
   | ListUsers !ListUsersOptions
   deriving (Show)
 
@@ -21,6 +23,7 @@ data ListCommand
 list :: ListCommand -> IO ()
 list (ListGroups opts) = listGroups opts
 list (ListPolicies opts) = listPolicies opts
+list (ListSessions opts) = listSessions opts
 list (ListUsers opts) = listUsers opts
 
 
@@ -30,6 +33,8 @@ listCommand = subparser
     (info (ListGroups <$> listGroupsOptions) (progDesc "List groups"))
   <> command "policies"
     (info (ListPolicies <$> listPoliciesOptions) (progDesc "List policies"))
+  <> command "sessions"
+    (info (ListSessions <$> listSessionsOptions) (progDesc "List sessions"))
   <> command "users"
     (info (ListUsers <$> listUsersOptions) (progDesc "List users"))
   )
