200c3e87f5eec635f55069d4ee5d2691b659cdad
Mike Taylor <mike.charles.taylor@gmail.com>
2024-04-17 03:19:04 -0600

    Fix tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,7 +6,6 @@ import Control.Monad.IO.Class
 import Control.Monad.Except
 import Crypto.Sign.Ed25519
 import Data.Aeson
-import Data.ByteString (ByteString)
 import Data.ByteString.Base64
 import Data.Text
 import Data.Text.Encoding
@@ -17,41 +16,54 @@ import Network.HTTP.Types
 import Test.Hspec
 import Test.Hspec.Wai
 
+import IAM.Group
+import IAM.GroupIdentifier
+import IAM.Policy
 import IAM.Server.API (app)
 import IAM.Server.Auth (stringToSign)
-import IAM.Server.IAM.DB
-import IAM.Server.IAM.DB.InMemory
-import IAM.Types
+import IAM.Server.Context
+import IAM.Server.DB
+import IAM.Server.DB.InMemory
+import IAM.Session hiding (createSession)
+import IAM.User
+import IAM.UserIdentifier
 
 
 main :: IO ()
 main = do
   db <- inMemory
   (pk, sk) <- createKeypair
-  callerPolicyId <- nextRandom
+  callerId <- UserUUID <$> nextRandom
+  callerPolicyId <- PolicyUUID <$> nextRandom
   let allowReads = Rule Allow Read "*"
       allowWrites = Rule Allow Write "*"
-      callerPolicy = Policy callerPolicyId callerPolicyRules
+      callerEmail = Just "caller@example.com"
+      callerPolicy = Policy callerPolicyId Nothing "localhost" callerPolicyRules
       callerPolicyRules = [allowReads, allowWrites]
-      callerId = UserEmail "caller@example.com"
-      callerPrincipal = User callerId [] [] [UserPublicKey pk "test"]
+      callerPrincipal = User callerId callerEmail [] [] [UserPublicKey pk "test"]
   result0 <- runExceptT $ createUser db callerPrincipal
   case result0 of
     Right _  -> do
       result1 <- runExceptT $ createPolicy db callerPolicy
       case result1 of
         Right _ -> do
-          result2 <- runExceptT $ createUserPolicyAttachment db callerId callerPolicyId
+          let cid = UserId callerId
+          let pid = PolicyId callerPolicyId
+          result2 <- runExceptT $ createUserPolicyAttachment db cid pid
           case result2 of
-            Right _ ->
-              hspec $ spec "localhost" db pk sk
+            Right _ -> do
+              result3 <- runExceptT $ createSession db callerId
+              case result3 of
+                Right callerSession ->
+                  hspec $ spec "localhost" db pk sk callerSession
+                Left _ -> error "Failed to create test session"
             Left _ -> error "Failed to attach test user policy"
         Left _ -> error "Failed to create test user policy"
     Left _ -> error "Failed to create test user"
 
 
-spec :: DB db => ByteString -> db -> PublicKey -> SecretKey -> Spec
-spec host db callerPK callerSK = with (return $ app host db) $ do
+spec :: DB db => Text -> db -> PublicKey -> SecretKey -> CreateSession -> Spec
+spec host db callerPK callerSK callerSession = with (return $ app host $ Ctx db) $ do
   describe "GET /users" $ do
     it "responds with 200" $ do
       requestId <- liftIO nextRandom
@@ -61,16 +73,19 @@ spec host db callerPK callerSK = with (return $ app host db) $ do
             , ("X-MTaylor-IO-User-Id", "caller@example.com")
             , ("X-MTaylor-IO-Public-Key", encodeUtf8 $ encodeBase64 $ unPublicKey callerPK)
             , ("X-MTaylor-IO-Request-Id", encodeUtf8 $ pack $ toString requestId)
+            , ("X-MTaylor-IO-Session-Token", encodeUtf8 $ createSessionToken callerSession)
             ]
           sig = encodeUtf8 $ encodeBase64 $ unSignature $ dsign callerSK authStringToSign
-          authStringToSign = stringToSign methodGet host "/users" "" requestId
+          authStringToSign =
+            stringToSign methodGet (encodeUtf8 host) "/users" "" requestId $
+              Just $ createSessionToken callerSession
       request methodGet "/users" headers mempty `shouldRespondWith` 200
   describe "POST /users with email address" $ do
     it "responds with 201" $ do
+      uid <- UserUUID <$> liftIO nextRandom
       (pk, _) <- liftIO createKeypair
       requestId <- liftIO nextRandom
-      let uid = UserEmail "bob@example.com"
-          user = User uid [] [] [UserPublicKey pk "test"]
+      let user = User uid Nothing [] [] [UserPublicKey pk "test"]
           userJSON = encode user
           headers =
             [ ("Authorization", "Signature " <> sig)
@@ -79,18 +94,21 @@ spec host db callerPK callerSK = with (return $ app host db) $ do
             , ("X-MTaylor-IO-User-Id", "caller@example.com")
             , ("X-MTaylor-IO-Public-Key", encodeUtf8 $ encodeBase64 $ unPublicKey callerPK)
             , ("X-MTaylor-IO-Request-Id", encodeUtf8 $ pack $ toString requestId)
+            , ("X-MTaylor-IO-Session-Token", encodeUtf8 $ createSessionToken callerSession)
             ]
           sig = encodeUtf8 $ encodeBase64 $ unSignature $ dsign callerSK authStringToSign
-          authStringToSign = stringToSign methodPost host "/users" "" requestId
+          authStringToSign =
+            stringToSign methodPost (encodeUtf8 host) "/users" "" requestId $
+              Just $ createSessionToken callerSession
       request methodPost "/users" headers userJSON `shouldRespondWith` 201
-      result <- liftIO $ runExceptT $ deleteUser db uid
-      liftIO $ result `shouldBe` Right uid
+      result <- liftIO $ runExceptT $ deleteUser db $ UserId uid
+      liftIO $ result `shouldBe` Right user
   describe "POST /users with UUID" $ do
     it "responds with 201" $ do
       uuid <- liftIO nextRandom
       (pk, _) <- liftIO createKeypair
       requestId <- liftIO nextRandom
-      let user = User (UserUUID uuid) [] [] [UserPublicKey pk "test"]
+      let user = User (UserUUID uuid) Nothing [] [] [UserPublicKey pk "test"]
           userJSON = encode user
           headers =
             [ ("Authorization", "Signature " <> sig)
@@ -99,12 +117,15 @@ spec host db callerPK callerSK = with (return $ app host db) $ do
             , ("X-MTaylor-IO-User-Id", "caller@example.com")
             , ("X-MTaylor-IO-Public-Key", encodeUtf8 $ encodeBase64 $ unPublicKey callerPK)
             , ("X-MTaylor-IO-Request-Id", encodeUtf8 $ pack $ toString requestId)
+            , ("X-MTaylor-IO-Session-Token", encodeUtf8 $ createSessionToken callerSession)
             ]
           sig = encodeUtf8 $ encodeBase64 $ unSignature $ dsign callerSK authStringToSign
-          authStringToSign = stringToSign methodPost host "/users" "" requestId
+          authStringToSign =
+            stringToSign methodPost (encodeUtf8 host) "/users" "" requestId $
+              Just $ createSessionToken callerSession
       request methodPost "/users" headers userJSON `shouldRespondWith` 201
-      result <- liftIO $ runExceptT $ deleteUser db $ UserUUID uuid
-      liftIO $ result `shouldBe` Right (UserUUID uuid)
+      result <- liftIO $ runExceptT $ deleteUser db $ UserId $ UserUUID uuid
+      liftIO $ result `shouldBe` Right user
   describe "GET /groups" $ do
     it "responds with 200" $ do
       requestId <- liftIO nextRandom
@@ -114,12 +135,16 @@ spec host db callerPK callerSK = with (return $ app host db) $ do
             , ("X-MTaylor-IO-User-Id", "caller@example.com")
             , ("X-MTaylor-IO-Public-Key", encodeUtf8 $ encodeBase64 $ unPublicKey callerPK)
             , ("X-MTaylor-IO-Request-Id", encodeUtf8 $ pack $ toString requestId)
+            , ("X-MTaylor-IO-Session-Token", encodeUtf8 $ createSessionToken callerSession)
             ]
           sig = encodeUtf8 $ encodeBase64 $ unSignature $ dsign callerSK authStringToSign
-          authStringToSign = stringToSign methodGet host "/groups" "" requestId
+          authStringToSign =
+            stringToSign methodGet (encodeUtf8 host) "/groups" "" requestId $
+              Just $ createSessionToken callerSession
       request methodGet "/groups" headers mempty `shouldRespondWith` 200
   describe "POST /groups" $ do
     it "responds with 201" $ do
+      gid <- GroupUUID <$> liftIO nextRandom
       requestId <- liftIO nextRandom
       let headers =
             [ ("Authorization", "Signature " <> sig)
@@ -128,13 +153,17 @@ spec host db callerPK callerSK = with (return $ app host db) $ do
             , ("X-MTaylor-IO-User-Id", "caller@example.com")
             , ("X-MTaylor-IO-Public-Key", encodeUtf8 $ encodeBase64 $ unPublicKey callerPK)
             , ("X-MTaylor-IO-Request-Id", encodeUtf8 $ pack $ toString requestId)
+            , ("X-MTaylor-IO-Session-Token", encodeUtf8 $ createSessionToken callerSession)
             ]
-          groupJSON = encode $ Group (GroupName "admins") [] []
+          group' = Group gid (Just "admins") [] []
+          groupJSON = encode group'
           sig = encodeUtf8 $ encodeBase64 $ unSignature $ dsign callerSK authStringToSign
-          authStringToSign = stringToSign methodPost host "/groups" "" requestId
+          authStringToSign =
+            stringToSign methodPost (encodeUtf8 host) "/groups" "" requestId $
+              Just $ createSessionToken callerSession
       request methodPost "/groups" headers groupJSON `shouldRespondWith` 201
       r <- liftIO $ runExceptT $ deleteGroup db $ GroupName "admins"
-      liftIO $ r `shouldBe` Right ()
+      liftIO $ r `shouldBe` Right group'
   describe "GET /policies" $ do
     it "responds with 200" $ do
       requestId <- liftIO nextRandom
@@ -144,14 +173,17 @@ spec host db callerPK callerSK = with (return $ app host db) $ do
             , ("X-MTaylor-IO-User-Id", "caller@example.com")
             , ("X-MTaylor-IO-Public-Key", encodeUtf8 $ encodeBase64 $ unPublicKey callerPK)
             , ("X-MTaylor-IO-Request-Id", encodeUtf8 requestIdString)
+            , ("X-MTaylor-IO-Session-Token", encodeUtf8 $ createSessionToken callerSession)
             ]
           requestIdString = pack $ toString requestId
           sig = encodeUtf8 $ encodeBase64 $ unSignature $ dsign callerSK authStringToSign
-          authStringToSign = stringToSign methodGet host "/policies" "" requestId
+          authStringToSign =
+            stringToSign methodGet (encodeUtf8 host) "/policies" "" requestId $
+              Just $ createSessionToken callerSession
       request methodGet "/policies" headers mempty `shouldRespondWith` 200
   describe "POST /policies" $ do
     it "responds with 201" $ do
-      pid <- liftIO nextRandom
+      pid <- PolicyUUID <$> liftIO nextRandom
       requestId <- liftIO nextRandom
       let headers =
             [ ("Authorization", "Signature " <> sig)
@@ -160,11 +192,14 @@ spec host db callerPK callerSK = with (return $ app host db) $ do
             , ("X-MTaylor-IO-User-Id", "caller@example.com")
             , ("X-MTaylor-IO-Public-Key", encodeUtf8 $ encodeBase64 $ unPublicKey callerPK)
             , ("X-MTaylor-IO-Request-Id", encodeUtf8 $ pack $ toString requestId)
+            , ("X-MTaylor-IO-Session-Token", encodeUtf8 $ createSessionToken callerSession)
             ]
-          policy = Policy pid [Rule Allow Read "*", Rule Allow Write "*"]
+          policy = Policy pid Nothing "localhost" [Rule Allow Read "*", Rule Allow Write "*"]
           policyJSON = encode policy
           sig = encodeUtf8 $ encodeBase64 $ unSignature $ dsign callerSK authStringToSign
-          authStringToSign = stringToSign methodPost host "/policies" "" requestId
+          authStringToSign =
+            stringToSign methodPost (encodeUtf8 host) "/policies" "" requestId $
+              Just $ createSessionToken callerSession
       request methodPost "/policies" headers policyJSON `shouldRespondWith` 201
-      r <- liftIO $ runExceptT $ deletePolicy db pid
+      r <- liftIO $ runExceptT $ deletePolicy db $ PolicyId pid
       liftIO $ r `shouldBe` Right policy
