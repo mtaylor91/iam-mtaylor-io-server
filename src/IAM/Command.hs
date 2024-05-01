1c5530470d014f3c9cda78398931754cfb9033b3
Mike Taylor <mike.charles.taylor@gmail.com>
2024-05-01 14:26:54 -0600

    Remove client code

--- src/IAM/Command.hs
+++ src/IAM/Command.hs
@@ -3,28 +3,10 @@ module IAM.Command ( options, run, ServerOptions(..) ) where
 
 import Options.Applicative
 
-import IAM.Command.Authorize
-import IAM.Command.Create
-import IAM.Command.Delete
-import IAM.Command.Get
-import IAM.Command.List
-import IAM.Command.Login
-import IAM.Command.Logout
-import IAM.Command.Keypair
 import IAM.Command.Server
 
 
-data Command
-  = Authorize !AuthorizeCommand
-  | Create !CreateCommand
-  | Delete !DeleteCommand
-  | Get !GetCommand
-  | List !ListCommand
-  | Login !LoginOptions
-  | Logout !LogoutOptions
-  | Keypair !KeypairOptions
-  | Server !ServerOptions
-  deriving (Show)
+newtype Command = Server ServerOptions deriving (Show)
 
 
 newtype Options = Options Command deriving (Show)
@@ -32,24 +14,7 @@ newtype Options = Options Command deriving (Show)
 
 options :: Parser Options
 options = Options <$> hsubparser
-  ( command "authorize"
-    (info (Authorize <$> authorizeCommand)
-    (progDesc "Authorize a user to perform an action"))
-  <> command "create"
-    (info (Create <$> createCommand) (progDesc "Create resources"))
-  <> command "delete"
-    (info (Delete <$> deleteCommand) (progDesc "Delete resources"))
-  <> command "get"
-    (info (Get <$> getCommand) (progDesc "Get resources"))
-  <> command "list"
-    (info (List <$> listCommand) (progDesc "List resources"))
-  <> command "login"
-    (info (Login <$> loginOptions) (progDesc "Login to the service"))
-  <> command "logout"
-    (info (Logout <$> logoutOptions) (progDesc "Logout of the service"))
-  <> command "keypair"
-    (info (Keypair <$> keypairOptions) (progDesc "Generate a keypair"))
-  <> command "server"
+  ( command "server"
     (info (Server <$> serverOptions) (progDesc "Start the server"))
   )
 
@@ -57,22 +22,6 @@ options = Options <$> hsubparser
 runOptions :: Options -> IO ()
 runOptions opts =
   case opts of
-    Options (Authorize cmd) ->
-      authorize cmd
-    Options (Create cmd) ->
-      create cmd
-    Options (Delete cmd) ->
-      delete cmd
-    Options (Get cmd) ->
-      get cmd
-    Options (List cmd) ->
-      list cmd
-    Options (Login opts') ->
-      login opts'
-    Options (Logout opts') ->
-      logout opts'
-    Options (Keypair opts') ->
-      keypair opts'
     Options (Server opts') ->
       server opts'
 
@@ -82,5 +31,5 @@ run = execParser opts >>= runOptions
   where
     opts = info (options <**> helper)
       ( fullDesc
-     <> header "api-mtaylor-io - API server for api.mtaylor.io service."
+     <> header "iam-mtaylor-io - API server for iam.mtaylor.io service."
       )
