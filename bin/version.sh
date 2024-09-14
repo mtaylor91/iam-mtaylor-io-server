1cc25606d8dca286adeb57bbfca231c8803e4231
Mike Taylor <mike.charles.taylor@gmail.com>
2024-09-13 22:17:14 -0600

    Tweak CI/helper scripts

--- bin/version.sh
+++ bin/version.sh
@@ -4,6 +4,3 @@ set -eux
 # Update the version in the package.yaml file
 sed -e "s/version:\(\s\+\).*/version:\1$1/g" < package.yaml > package.yaml.tmp
 mv package.yaml.tmp package.yaml
-
-# Update the version in the kustomization.yaml file
-(cd deploy && kustomize edit set image "images.home.mtaylor.io/iam-mtaylor-io:$1")
