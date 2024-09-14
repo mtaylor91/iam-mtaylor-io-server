770efc5fb43a1179bbe0b31c23cd468aea1598f7
Mike Taylor <mike.charles.taylor@gmail.com>
2024-09-13 22:13:03 -0600

    WIP: version tagging

--- bin/version.sh
+++ bin/version.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 set -eux
-cat package.yaml | sed -e "s/version:\(\s\+\).*/version:\1$1/g" > package.yaml.tmp
+
+# Update the version in the package.yaml file
+sed -e "s/version:\(\s\+\).*/version:\1$1/g" < package.yaml > package.yaml.tmp
 mv package.yaml.tmp package.yaml
+
+# Update the version in the kustomization.yaml file
+(cd deploy && kustomize edit set image "images.home.mtaylor.io/iam-mtaylor-io:$1")
